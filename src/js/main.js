3024d282a9d523dd21b17eb581759d03f52d8ed2
voskanvl <voskanvl@gmail.com>
2022-07-21 22:14:53 +0300

    add splide

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log("start");
+import Splide from "@splidejs/splide"
+
+if (document.readyState === "loading") {
+    start()
+} else {
+    document.addEventListener("DOMContentLoaded", start)
+}
+
+function start() {
+    console.log("start")
+    new Splide(".splide", {
+        type: "loop",
+        perPage: 2,
+    }).mount()
+}
