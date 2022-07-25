fc1f0d30395dfba078493e10dce897b8b7d69dea
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-07-25 14:43:48 +0300

    fix constructor splide in mail

--- src/js/main.js
+++ src/js/main.js
@@ -10,13 +10,25 @@ window.onload = start;
 
 function start() {
     console.log("start");
-    new Splide("#splideReview", {
+
+    let splideReview = new Splide("#splideReview", {
         type: "loop",
-        perPage: 2,
         gap: "6.197917vw",
+        fixedWidth: "clamp(370px,48vw, 603px",
         arrowPath: " ",
     }).mount();
     new Splide("#splideTiser", {
         perPage: 1,
+        rewind: false,
     }).mount();
+
+    // window.onresize = () => {
+    //     const match = matchMedia("max-width: 770px");
+    //     document.querySelector("#splideReview").inn
+    //     splideReview = new Splide("#splideReview", {
+    //         type: "loop",
+    //         perPage: 1,
+    //         arrowPath: " ",
+    //     }).mount();
+    // };
 }
