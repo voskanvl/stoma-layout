1088b9c91143641ca06ce9020787d9ea0edde1eb
voskanvl <voskanvl@gmail.com>
2022-07-28 21:08:18 +0300

    refactoring validation

--- src/js/validation.js
+++ src/js/validation.js
@@ -1,37 +1,37 @@
-import Inputmask from "inputmask";
+import Inputmask from "inputmask"
 function add(type, validator) {}
 export function Validation(form) {
     const formElement =
         typeof form === "object" && form.constructor.name === "HTMLFormElement"
             ? form
-            : document.querySelector(form);
+            : document.querySelector(form)
     if (!formElement)
-        throw Error("argument type have to be string or HTMLFormElement");
+        throw Error("argument type have to be string or HTMLFormElement")
 
     const required = [...formElement.elements].filter(el =>
         el.getAttribute("required"),
-    );
+    )
 
-    const group = {};
-    group.phone = required.filter(el => el.getAttribute("type") === "tel");
-    group.text = required.filter(el => el.getAttribute("type") === "text");
-    group.email = required.filter(el => el.getAttribute("type") === "email");
+    const group = {}
+    group.phone = required.filter(el => el.getAttribute("type") === "tel")
+    group.text = required.filter(el => el.getAttribute("type") === "text")
+    group.email = required.filter(el => el.getAttribute("type") === "email")
     group.checkbox = required.filter(
         el => el.getAttribute("type") === "checkbox",
-    );
+    )
     group.submit = [...formElement.elements].filter(
         el => el.getAttribute("type") === "submit",
-    );
+    )
 
-    const isValid = () => formElement.checkValidity();
+    const isValid = () => formElement.checkValidity()
 
     function add(type, validator) {
-        if (group[type]) group[type].forEach(e => validator.mask(e));
+        if (group[type]) group[type].forEach(e => validator.mask(e))
     }
 
     return {
         get valid() {
-            return formElement.checkValidity();
+            return formElement.checkValidity()
         },
         data: {
             phone: group.phone,
@@ -40,11 +40,12 @@ export function Validation(form) {
             checkbox: group.checkbox,
             submit: group.submit,
         },
-        addMask({ phone, text, email, checkbox }) {
-            if (phone) add("phone", phone);
-            if (text) add("text", text);
-            if (email) addPh("email", email);
-            if (checkbox) add("checkbox", checkbox);
+        addMask(props) {
+            const avaibleFields = ["phone", "text", "email", "checkbox"]
+            if (typeof props === "object")
+                Object.keys(props).forEach(e => {
+                    if (e in avaibleFields) add(e, props[e])
+                })
         },
-    };
+    }
 }
