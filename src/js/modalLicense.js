3c305666e9323b256fd0b3226bf630bb9f344376
Mike-05 <murodjontoshpulotovofficial@gmail.com>
2022-07-28 12:04:21 +0300

    license done

--- src/js/modalLicense.js
+++ src/js/modalLicense.js
@@ -1,15 +1,29 @@
 export function startLicense() {
     console.log("startLicense");
     const imgs = document.querySelectorAll(".licenses__img");
+    const containerModalLicense = document.querySelector(
+        ".modalLicense__container",
+    );
     const targetImg = document.querySelector(".modalLicense__img");
+    const modalLicenseClose = document.querySelector(".modalLicense__close");
+    const modalLicenseDownload = document.querySelector(
+        ".modalLicense__download",
+    );
     imgs.forEach(img =>
         img.addEventListener("click", ({ currentTarget }) => {
             if ("dataset" in currentTarget && "id" in currentTarget.dataset) {
                 const { id } = currentTarget.dataset;
+                containerModalLicense.removeAttribute("hidden");
                 targetImg.src = `../../assets/images/licenses/800/00${
+                    +id + 1
+                }.jpg`;
+                modalLicenseDownload.href = `../../assets/images/licenses/800/00${
                     +id + 1
                 }.jpg`;
             }
         }),
     );
+    modalLicenseClose.addEventListener("click", () => {
+        containerModalLicense.setAttribute("hidden", "hidden");
+    });
 }
