ba68972027e93df1d130e8c1e7864daa0946aa51
voskanvl <voskanvl@gmail.com>
2022-07-30 15:05:51 +0300

    insert livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,38 @@
-const { src, dest, series, parallel, watch } = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
-const del = require("del");
-const pug = require("gulp-pug");
+const { src, dest, series, parallel, watch } = require("gulp")
+const sass = require("gulp-sass")(require("sass"))
+const del = require("del")
+const pug = require("gulp-pug")
 // const include = require("gulp-include");
-const webpack = require("webpack-stream");
+const webpack = require("webpack-stream")
 // const named = require("vinyl-named");
-const sourcemaps = require("gulp-sourcemaps");
-const browserSync = require("browser-sync").create();
-const fs = require("fs/promises");
-const { readFileSync } = require("fs");
-const path = require("path");
-const svgSprite = require("gulp-svg-sprite");
-const through2 = require("through2");
+const sourcemaps = require("gulp-sourcemaps")
+const browserSync = require("browser-sync").create()
+const fs = require("fs/promises")
+const { readFileSync } = require("fs")
+const path = require("path")
+const svgSprite = require("gulp-svg-sprite")
+const through2 = require("through2")
 // const gutil = require("gulp-util")
-const Vinyl = require("vinyl");
-const liveServer = require("live-server");
+const Vinyl = require("vinyl")
+const liveServer = require("live-server")
+const server = require("gulp-server-livereload")
 
 const injectJSON = async () => {
-    let dataFromFiles = await fs.readdir("./src/data");
+    let dataFromFiles = await fs.readdir("./src/data")
     dataFromFiles = await dataFromFiles
         .filter(file => file.split(".").pop() === "json")
         .map(async file => {
-            const raw = await fs.readFile(path.resolve("./src/data", file));
-            return JSON.parse(raw);
-        });
-    dataFromFiles = await Promise.all(dataFromFiles);
-    return dataFromFiles.reduce((acc, e) => ({ ...acc, ...e }), {});
-};
+            const raw = await fs.readFile(path.resolve("./src/data", file))
+            return JSON.parse(raw)
+        })
+    dataFromFiles = await Promise.all(dataFromFiles)
+    return dataFromFiles.reduce((acc, e) => ({ ...acc, ...e }), {})
+}
 
 const clean = path => cb => {
-    del([path]);
-    cb();
-};
+    del([path])
+    cb()
+}
 
 // const sync = () =>
 //     browserSync.init({
@@ -56,7 +57,7 @@ const svgSpriteTask = cb =>
                 },
             }),
         )
-        .pipe(dest("./dist"));
+        .pipe(dest("./dist"))
 
 const jsTask = cb =>
     src("./src/js/main.js")
@@ -67,10 +68,10 @@ const jsTask = cb =>
             }),
         )
         .pipe(sourcemaps.write("."))
-        .pipe(dest("./dist"));
+        .pipe(dest("./dist"))
 
 const pugTask = async () => {
-    const json = await injectJSON();
+    const json = await injectJSON()
     src("./src/pug/*.pug")
         .pipe(
             pug({
@@ -80,17 +81,17 @@ const pugTask = async () => {
         )
         .pipe(
             through2.obj(function (file, _, cb) {
-                console.log("🚀 ~ file", file.path);
+                console.log("🚀 ~ file", file.path)
                 const iterator = file.contents
                     .toString()
-                    .matchAll(/(?<=<img).*(src="(.*?)").*(?=>)/g);
-                let files = [...iterator].map(e => e[2]);
+                    .matchAll(/(?<=<img).*(src="(.*?)").*(?=>)/g)
+                let files = [...iterator].map(e => e[2])
                 for (let i of files) {
                     const pathImg = path.resolve(
                         file.history[file.history.length - 2],
                         i,
-                    );
-                    const data = readFileSync(pathImg);
+                    )
+                    const data = readFileSync(pathImg)
                     this.push(
                         new Vinyl({
                             base: file.base,
@@ -101,52 +102,71 @@ const pugTask = async () => {
                             ),
                             contents: data,
                         }),
-                    );
+                    )
                 }
-                cb(null, file);
+                cb(null, file)
             }),
         )
-        .pipe(dest("./dist"));
-};
+        .pipe(dest("./dist"))
+}
 
 const sassTask = cb =>
     src("./src/style/style.sass")
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(sass())
         .pipe(sourcemaps.write("."))
-        .pipe(dest("./dist"));
+        .pipe(dest("./dist"))
 
 const watchTask = () => {
-    watch("./src/js/*.js", series(clean("./dist/main.js"), jsTask));
-    watch("./src/components/**/*", series(clean("./dist/*.html"), pugTask));
-    watch("./src/pug/**/*.pug", series(clean("./dist/*.html"), pugTask));
-    watch("./src/**/*.sass", series(clean("./dist/*.css"), sassTask));
-    watch("./src/data/**/*.json", series(clean("./dist/*.html"), pugTask));
+    watch("./src/js/*.js", series(clean("./dist/main.js"), jsTask))
+    watch("./src/components/**/*", series(clean("./dist/*.html"), pugTask))
+    watch("./src/pug/**/*.pug", series(clean("./dist/*.html"), pugTask))
+    watch("./src/**/*.sass", series(clean("./dist/*.css"), sassTask))
+    watch("./src/data/**/*.json", series(clean("./dist/*.html"), pugTask))
     watch(
         "./src/assets/svg/**/*.svg",
         series(clean("./dist/*.svg"), svgSpriteTask),
-    );
-};
+    )
+}
 
 // exports.sync = sync;
 
-exports.js = jsTask;
+exports.js = jsTask
 
-exports.pug = pugTask;
+exports.pug = pugTask
 
-exports.watch = watchTask;
+exports.watch = watchTask
 
-exports.sprite = svgSpriteTask;
+exports.sprite = svgSpriteTask
 
-exports.watch = watchTask;
+exports.watch = watchTask
 
 // exports.browserSync = parallel(sync, watchTask);
-exports.live = parallel(() => liveServer.start({ root: "./dist" }), watchTask);
+
+const reload = () => {
+    src("./dist/").pipe(
+        server({
+            livereload: true,
+            directoryListing: {
+                path: "./dist",
+            },
+            port: 9100,
+        }),
+    )
+}
 
 const defaultTask = parallel(
     series(clean("./dist/main.js"), jsTask),
     series(clean("./dist/*.html"), pugTask),
     series(clean("./dist/*.css"), sassTask),
     series(clean("./dist/*.svg"), svgSpriteTask),
-);
-exports.default = defaultTask;
+)
+exports.default = defaultTask
+
+exports.live = series(
+    defaultTask,
+    parallel(watchTask, () => liveServer.start({ root: "./dist" })),
+)
+exports.reload = reload
+
+exports.livereload = series(defaultTask, reload, watchTask)
